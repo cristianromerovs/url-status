7b6f452e609f885329547f186bc79228e0715065
cristianromerovs <cristianromerovs@gmail.com>
2024-07-30 16:36:43 -0400

    async execute implementation

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ const https = require('https');
 const fs = require('fs');
 const domain = 'www.entel.pe';
 
-fs.readFile('rutas.txt', 'utf8', (err, data) => {
+function fetchUrl(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      console.log(`${response.statusCode}`);
+      resolve();
+    }).on('error', (error) => {
+      console.error(`Error al intentar acceder a ${url}: ${error.message}`);
+      resolve(); // Resolvemos para que continue con la siguiente ruta incluso si hay un error
+    });
+  });
+}
+
+fs.readFile('rutas.txt', 'utf8', async (err, data) => {
   if (err) {
     console.error('Error al leer el archivo:', err);
     return;
   }
   const routes = data.split('\n').map(route => route.trim());
 
-  routes.forEach((route, index) => {
+  for (const route of routes) {
     const fullUrl = `https://${domain}${route}`;
-    const request = https.get(fullUrl, (response) => {
-        console.log(`${fullUrl}, ${response.statusCode}`);
-    });
-
-    request.on('error', (error) => {
-      console.error(`Error al intentar acceder a ${fullUrl}: ${error.message}`);
-    });
-  });
+    await fetchUrl(fullUrl);
+  }
 });
